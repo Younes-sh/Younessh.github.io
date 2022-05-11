57e74b66c6f116c4c73be50c245e34eec5977b6b
younes sheikhlar <sheikhlaryounes@mail.com>
2022-05-11 22:45:14 +0200

    Branch JSmenu_burger : Burger menu codes were written

---
+++ JavaScript/index.js
@@ -0,0 +1,7 @@
+const btnMenu = document.querySelector('.btn-menu');
+const menuBurger = document.querySelector('.menu-burger')
+btnMenu.addEventListener('click',menuburger);
+function menuburger(){
+    btnMenu.classList.toggle('open');
+    menuBurger.classList.toggle('visibleHiden')
+}
